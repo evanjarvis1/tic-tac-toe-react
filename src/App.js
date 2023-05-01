78de7cfe2622f358a5acf163dbf80cbad4e04ca4
Evan <evanlegee@gmail.com>
2023-05-01 06:01:38 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
+import Square from "./Square";
 
 function App() {
+  let variableOfMyChoice = `String of my choice`;
+
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+
+  const [player, setPlayer] = useState(true);
+
+  const handleReset = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""]);
+    setPlayer(true);
+  };
+
+  function calculateWinner(arr) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+        return `${arr[a]} wins!`;
+      }
+    }
+    return "";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {squares.map((value, index) => {
+          return (
+            <Square
+              squares={squares}
+              setSquares={setSquares}
+              player={player}
+              setPlayer={setPlayer}
+              squareValue={value}
+              index={index}
+            />
+          );
+        })}
+      </div>
+      <div className="reset-button-div">
+        <button className="reset-button" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
+      <span className="">{calculateWinner(squares)}</span>
     </div>
   );
 }
